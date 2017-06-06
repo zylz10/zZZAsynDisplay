e1f1f74f3320e7d455750e63f8dd7a697361bb75
zylz10 <zhangyiling@nicoledeimac.local>
2017-06-06 10:55:04 +0800

    Initial Commit

---
+++ zZZAsynDisplay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zZZAsynDisplay
+//
+//  Created by zhangyiling on 17/6/6.
+//  Copyright © 2017年 zhangyiling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zZZAsynDisplay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zZZAsynDisplay
+//
+//  Created by zhangyiling on 17/6/6.
+//  Copyright © 2017年 zhangyiling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
